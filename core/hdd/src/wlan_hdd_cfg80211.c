baaa748cc9dc41fad534f0d944a92addb50cd72b
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-08-04 11:22:54 -0700

    qcacld-3.0: Add support for CFG80211_REMOVE_IEEE80211_BACKPORT
    Add flag CFG80211_REMOVE_IEEE80211_BACKPORT and also introduce
    HDD_NUM_NL80211_BANDS.
    
    Change-Id: I60a15a1273f939a0a288d08d03ccb5bf1bdcc429
    CRs-Fixed: 2087917

--- core/hdd/src/wlan_hdd_cfg80211.c
+++ core/hdd/src/wlan_hdd_cfg80211.c
@@ -11712,7 +11712,7 @@ int wlan_hdd_cfg80211_update_band(struct wiphy *wiphy, eCsrBand eBand)
 
 	ENTER();
 
-	for (i = 0; i < NUM_NL80211_BANDS; i++) {
+	for (i = 0; i < HDD_NUM_NL80211_BANDS; i++) {
 
 		if (NULL == wiphy->bands[i])
 			continue;
@@ -11979,7 +11979,7 @@ int wlan_hdd_cfg80211_init(struct device *dev,
 			sizeof(hdd_channels_5_ghz));
 	}
 
-	for (i = 0; i < NUM_NL80211_BANDS; i++) {
+	for (i = 0; i < HDD_NUM_NL80211_BANDS; i++) {
 
 		if (NULL == wiphy->bands[i])
 			continue;
@@ -12069,7 +12069,7 @@ void wlan_hdd_cfg80211_deinit(struct wiphy *wiphy)
 {
 	int i;
 
-	for (i = 0; i < NUM_NL80211_BANDS; i++) {
+	for (i = 0; i < HDD_NUM_NL80211_BANDS; i++) {
 		if (NULL != wiphy->bands[i] &&
 		   (NULL != wiphy->bands[i]->channels)) {
 			qdf_mem_free(wiphy->bands[i]->channels);
