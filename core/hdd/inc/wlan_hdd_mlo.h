0de141916dda38bf0d438960f8e4080799870eca
Bapiraju Alla <quic_balla@quicinc.com>
2021-10-11 23:22:26 +0530

    qcacld-3.0: Add support to dynamic MAC address update
    Currently, MAC address update is supported only when interface is down.
    Because of this framework needs to issue interface down and interface
    up to update the MAC address. Interface down and up will lead to VDEV
    delete and VDEV create commands to the FW.
    
    To optimize Host-FW interactions during set MAC address, add support
    to update the MAC address without bringing the interface to down state.
    
    Change-Id: I76afa8f8c2504e8271bf689392d526f994afff0a
    CRs-Fixed: 3063201

--- core/hdd/inc/wlan_hdd_mlo.h
+++ core/hdd/inc/wlan_hdd_mlo.h
@@ -42,9 +42,12 @@ struct hdd_adapter_create_param {
 #if defined(WLAN_FEATURE_11BE_MLO) && defined(CFG80211_11BE_BASIC)
 #define hdd_adapter_is_link_adapter(x) ((x)->mlo_adapter_info.is_link_adapter)
 #define hdd_adapter_is_ml_adapter(x)   ((x)->mlo_adapter_info.is_ml_adapter)
+#define hdd_adapter_is_associated_with_ml_adapter(x) \
+			   ((x)->mlo_adapter_info.associate_with_ml_adapter)
 #else
 #define hdd_adapter_is_link_adapter(x) (0)
 #define hdd_adapter_is_ml_adapter(x)   (0)
+#define hdd_adapter_is_associated_with_ml_adapter(x) (0)
 #endif
 
 #if defined(WLAN_FEATURE_11BE_MLO) && defined(CFG80211_11BE_BASIC)
