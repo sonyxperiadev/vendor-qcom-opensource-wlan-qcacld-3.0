427122e399cfdf80735005abca8984adf7cf545d
Will Huang <quic_wilhuang@quicinc.com>
2021-12-07 16:42:45 +0800

    qcacld-3.0: Skip num_vdev-1 if disable NAN feature
    Currently if disable NAN support in host and fw, max vdev will still
    decrease 1, which make it unable to support 4 SAP currency case.
    
    Fix it by checking WMI_SERVICE_NAN capability, if don't have to support
    NAN feature, skip reserve vdev for NAN in host.
    
    Change-Id: I771a8678d285cbbfc2f92598ac63a27a989ac5a0
    CRs-Fixed: 3091454

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -6827,12 +6827,14 @@ int wma_rx_service_ready_ext_event(void *handle, uint8_t *event,
 	 * the num_vdevs by 1.
 	 */
 
-	if (ucfg_nan_is_vdev_creation_allowed(wma_handle->psoc) ||
-	    QDF_GLOBAL_FTM_MODE == cds_get_conparam()) {
-		wlan_res_cfg->nan_separate_iface_support = true;
-	} else {
-		wlan_res_cfg->num_vdevs--;
-		wma_update_num_peers_tids(wma_handle, wlan_res_cfg);
+	if (wmi_service_enabled(wma_handle->wmi_handle, wmi_service_nan)) {
+		if (ucfg_nan_is_vdev_creation_allowed(wma_handle->psoc) ||
+		    QDF_GLOBAL_FTM_MODE == cds_get_conparam()) {
+			wlan_res_cfg->nan_separate_iface_support = true;
+		} else {
+			wlan_res_cfg->num_vdevs--;
+			wma_update_num_peers_tids(wma_handle, wlan_res_cfg);
+		}
 	}
 
 	if ((ucfg_pkt_capture_get_mode(wma_handle->psoc) !=
