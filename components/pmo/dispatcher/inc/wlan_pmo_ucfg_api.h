feaeb90de9a65454ac2f6b8e45ddb0e991ddf0ff
Amit Mehta <quic_amitmeht@quicinc.com>
2022-02-13 15:55:23 +0530

    qcacld-3.0: Print wow_nack reason code
    Currently, Fw did not set NACK reason code
    to Host when Fw is not ready for WoW.
    
    As part of the new change, Fw will send NACK
    reason code along with WoW NACK to host.
    Make changes in host to store and print
    NACK reason code.
    
    Change-Id: Ic978af9572c8de37df3fb926083aac5e6ddefda7
    CRs-Fixed: 3481177

--- components/pmo/dispatcher/inc/wlan_pmo_ucfg_api.h
+++ components/pmo/dispatcher/inc/wlan_pmo_ucfg_api.h
@@ -1076,11 +1076,14 @@ int ucfg_pmo_psoc_clear_target_wake_up(struct wlan_objmgr_psoc *psoc);
 
 /**
  * ucfg_pmo_psoc_target_suspend_acknowledge() - Clear initial wake up status
- * @psoc: objmgr psoc handle
+ * @context: caller-provided context
+ * @wow_nack: Was WoW NACK'ed
+ * @reason_code: WoW status reason code
  *
  * Return: None
  */
-void ucfg_pmo_psoc_target_suspend_acknowledge(void *context, bool wow_nack);
+void ucfg_pmo_psoc_target_suspend_acknowledge(void *context, bool wow_nack,
+					      uint16_t reason_code);
 
 /**
  * ucfg_pmo_psoc_wakeup_host_event_received() - got host wake up evennt from fwr
@@ -1873,7 +1876,8 @@ ucfg_pmo_psoc_clear_target_wake_up(struct wlan_objmgr_psoc *psoc)
 }
 
 static inline void
-ucfg_pmo_psoc_target_suspend_acknowledge(void *context, bool wow_nack)
+ucfg_pmo_psoc_target_suspend_acknowledge(void *context, bool wow_nack,
+					 uint16_t reason_code)
 {
 }
 
