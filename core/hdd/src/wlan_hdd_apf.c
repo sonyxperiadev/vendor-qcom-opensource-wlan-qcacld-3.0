c9999b3b40cb15b67d44e6b67e68e903958981f0
Alan Chen <quic_alache@quicinc.com>
2022-02-17 10:45:30 -0800

    qcacld-3.0: Use hdd_enter_dev() instead of hdd_enter() to set APF offload
    Use hdd_enter_dev() instead of hdd_enter() to set APF offload so that
    it will print out which vdev it is coming from.
    
    Change-Id: If75d835f39c86f149c1f42c4289b45083fa84f09
    CRs-Fixed: 3133175

--- core/hdd/src/wlan_hdd_apf.c
+++ core/hdd/src/wlan_hdd_apf.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -608,7 +609,7 @@ __wlan_hdd_cfg80211_apf_offload(struct wiphy *wiphy,
 	int ret_val = 0, apf_subcmd;
 	struct hdd_apf_context *context;
 
-	hdd_enter();
+	hdd_enter_dev(dev);
 
 	if (!adapter) {
 		hdd_err("Adapter is null");
