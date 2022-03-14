2bb23a6dd79d33b8ab17173cf116a50208483484
Pragaspathi Thilagaraj <quic_tpragasp@quicinc.com>
2022-03-14 12:13:10 +0530

    qcacld-3.0: Add new Wifi Pos files for legacy ops
    Add below new Wifi pos files to register legacy peer create and
    peer delete operations:
    core/inc/wlan_wifi_pos_interface.h
    core/src/wlan_wifi_pos_interface.c
    dispatcher/inc/wifi_pos_ucfg_api.h
    dispatcher/src/wifi_pos_ucfg_api.c
    
    Change-Id: Iee628450fbaac9d57f1b672ca0550ce5c828f7d5
    CRs-Fixed: 3170664

---
+++ components/wifi_pos/core/inc/wlan_wifi_pos_interface.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+/**
+ * DOC: declare WIFI POS interface APIs exposed by the mlme component
+ */
+
+#ifndef _WLAN_WIFI_POS_IFACE_API_H_
+#define _WLAN_WIFI_POS_IFACE_API_H_
+
+#include <wlan_objmgr_vdev_obj.h>
+#include "wma_if.h"
+
+#if defined(WIFI_POS_CONVERGED) && defined(WLAN_FEATURE_RTT_11AZ_SUPPORT)
+/**
+ * wifi_pos_register_legacy_ops() - Register wifi pos legacy callbacks
+ * @psoc: Psoc pointer
+ *
+ * Return: QDF_STATUS
+ */
+QDF_STATUS
+wifi_pos_register_legacy_ops(struct wlan_objmgr_psoc *psoc);
+
+/**
+ * ucfg_wifi_pos_set_legacy_ops() - Wrapper to set legacy ops
+ * @psoc: PSOC pointer
+ *
+ * Return: QDF_STATUS
+ */
+static inline QDF_STATUS
+ucfg_wifi_pos_set_legacy_ops(struct wlan_objmgr_psoc *psoc)
+{
+	return wifi_pos_register_legacy_ops(psoc);
+}
+#else
+static inline QDF_STATUS
+ucfg_wifi_pos_set_legacy_ops(struct wlan_objmgr_psoc *psoc)
+{
+	return QDF_STATUS_SUCCESS;
+}
+#endif
+#endif /* _WLAN_WIFI_POS_IFACE_API_H_ */
