d4498674f3cb89733a9b778584f85d00267ff53d
Manikandan Mohan <quic_manikand@quicinc.com>
2022-05-11 14:47:50 -0700

    qcacld-3.0: Add config file for Mango WLAN device
    Add config file for Mango WLAN device and enable emulation flag for
    Mango WLAN device.
    
    In addition, use static symbolic link '.mango -> .' for multi-ko support.
    
    Change-Id: I0cebf11407e5625fd6475723496b9cda72cf5165
    CRs-Fixed: 3210120

--- core/pld/src/pld_pcie.c
+++ core/pld/src/pld_pcie.c
@@ -641,7 +641,11 @@ static struct pci_device_id pld_pcie_id_table[] = {
 #elif defined(QCA_WIFI_QCA6490)
 	{ 0x17cb, 0x1103, PCI_ANY_ID, PCI_ANY_ID },
 #elif defined(QCA_WIFI_KIWI)
+#if defined(QCA_WIFI_MANGO)
+	{ 0x17cb, 0x110A, PCI_ANY_ID, PCI_ANY_ID },
+#else
 	{ 0x17cb, 0x1107, PCI_ANY_ID, PCI_ANY_ID },
+#endif
 #elif defined(QCN7605_SUPPORT)
 	{ 0x17cb, 0x1102, PCI_ANY_ID, PCI_ANY_ID },
 #else
