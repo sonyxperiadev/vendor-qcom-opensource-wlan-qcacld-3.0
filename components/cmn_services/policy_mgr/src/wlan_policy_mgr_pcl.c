8bcb061a89e9482b3daceaefa7e285c3dbaefa3c
Liangwei Dong <quic_liangwei@quicinc.com>
2022-06-07 10:02:10 +0800

    qcacld-3.0: Fix 3 home channel issue for PCL PM_SBS_CH_2G
    New PCL PCL PM_SBS_CH_2G,PM_SCC_ON_5G_LOW_5G_LOW,
    PM_SCC_ON_5G_HIGH_5G_HIGH do not allow 3 home channel
    scenario. Remove it from API
    policy_mgr_is_3rd_conn_on_same_band_allowed.
    Add PCL string print support for new type:
    PM_SCC_ON_5G_LOW_5G_LOW, PM_SCC_ON_5G_HIGH_5G_HIGH.
    
    Change-Id: Ie6dbb042f5ecb04cb999a7949ced8e7c7ab4ad5a
    CRs-Fixed: 3215568

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
@@ -3497,9 +3497,6 @@ bool policy_mgr_is_3rd_conn_on_same_band_allowed(struct wlan_objmgr_psoc *psoc,
 	case PM_24G_MCC_CH:
 	case PM_5G_MCC_CH:
 	case PM_24G_SBS_CH_MCC_CH:
-	case PM_SBS_CH_2G:
-	case PM_SCC_ON_5G_LOW_5G_LOW:
-	case PM_SCC_ON_5G_HIGH_5G_HIGH:
 		ret = true;
 		break;
 	default:
