03622c131b9952f5f481a2067e93d8b39447c65c
Yu Wang <quic_yyuwang@quicinc.com>
2022-06-27 17:33:40 +0800

    qcacld-3.0: add support for VA based minidump
    Initialize/De-initialize minidump upon module loading/unloading.
    
    Change-Id: I333434312d8cc7080f81957b9b40b3138783145c
    CRs-Fixed: 3234894

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -17177,6 +17177,7 @@ static QDF_STATUS hdd_qdf_init(void)
 	}
 
 	qdf_trace_init();
+	qdf_minidump_init();
 	qdf_register_debugcb_init();
 
 	return QDF_STATUS_SUCCESS;
@@ -17204,7 +17205,7 @@ exit:
 static void hdd_qdf_deinit(void)
 {
 	/* currently, no debugcb deinit */
-
+	qdf_minidump_deinit();
 	qdf_trace_deinit();
 
 	/* currently, no trace spinlock deinit */
