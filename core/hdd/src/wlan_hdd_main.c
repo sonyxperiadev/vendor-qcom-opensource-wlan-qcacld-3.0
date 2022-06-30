c0956f31ba3541663f9a89fcc36b81519f287125
Ashish <quic_adhanoti@quicinc.com>
2022-06-30 21:51:09 +0530

    qcacld-3.0: Update connected APs beacon IE correctly
    Currently host driver does not update connected APs beacon
    IE and because of this host is not able to pass this information
    to userspace when it request for this info.
    
    To address above issue, update connected APs beacon IE
    correclty in host driver.
    
    Change-Id: Id08d0162704883ad8ccb0f1c436aa8f9886f28c6
    CRs-Fixed: 3193823

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -6619,6 +6619,7 @@ void hdd_cleanup_conn_info(struct hdd_adapter *adapter)
 {
 	struct hdd_station_ctx *hdd_sta_ctx =
 					WLAN_HDD_GET_STATION_CTX_PTR(adapter);
+	struct element_info *bcn_ie;
 
 	if (!hdd_sta_ctx)
 		return;
@@ -6626,6 +6627,13 @@ void hdd_cleanup_conn_info(struct hdd_adapter *adapter)
 	if (hdd_sta_ctx->cache_conn_info.he_operation) {
 		qdf_mem_free(hdd_sta_ctx->cache_conn_info.he_operation);
 		hdd_sta_ctx->cache_conn_info.he_operation = NULL;
+	}
+	bcn_ie = &hdd_sta_ctx->conn_info.prev_ap_bcn_ie;
+
+	if (bcn_ie->ptr) {
+		qdf_mem_free(bcn_ie->ptr);
+		bcn_ie->ptr = NULL;
+		bcn_ie->len = 0;
 	}
 }
 
