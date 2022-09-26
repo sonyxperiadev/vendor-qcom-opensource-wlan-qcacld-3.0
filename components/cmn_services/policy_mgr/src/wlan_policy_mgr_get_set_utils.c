eb71eaa92081a4d7e5556cbc075c4f80c54946d4
Arun Kumar Khandavalli <quic_akhandav@quicinc.com>
2022-09-26 20:50:38 +0530

    qcacld-3.0: Return success if underlying target is not supporting sbs
    Return success instead of not supported when the underlying hardware
    is not supporting SBS
    
    Change-Id: I3bb890ca2e8f096a918e2e718d44980ccdf53147
    CRs-Fixed: 3300183

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_get_set_utils.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_get_set_utils.c
@@ -1578,7 +1578,7 @@ QDF_STATUS policy_mgr_update_sbs_freq(struct wlan_objmgr_psoc *psoc,
 	policy_mgr_update_sbs_lowr_band_end_frq(pm_ctx, info);
 	/* no need to update if sbs_lower_band_end_freq is not set */
 	if (!pm_ctx->hw_mode.sbs_lower_band_end_freq)
-		return QDF_STATUS_E_NOSUPPORT;
+		return QDF_STATUS_SUCCESS;
 
 	policy_mgr_update_hw_mode_list(psoc, tgt_hdl);
 
