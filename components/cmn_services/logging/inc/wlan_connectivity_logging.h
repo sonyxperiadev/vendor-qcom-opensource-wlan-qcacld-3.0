53b00c93a36279c990108d10b105a4825b1bcb84
vijaraj <quic_vijaraj@quicinc.com>
2022-10-17 06:51:57 -0700

    qcacld-3.0: Add diag logging support for EAPOL start
    Add new entry in the enum qca_conn_diag_log_event_type in
    order to support EAPOL start packet.
    
    Change-Id: I540ef257ee9425eef016302648844ebf01647372
    CRs-Fixed: 3314689

--- components/cmn_services/logging/inc/wlan_connectivity_logging.h
+++ components/cmn_services/logging/inc/wlan_connectivity_logging.h
@@ -160,6 +160,7 @@ enum wlan_main_tag {
  * @WLAN_CONN_DIAG_EAP_SUCC_EVENT: EAP success
  * @WLAN_CONN_DIAG_EAP_FAIL_EVENT: EAP failure
  * @WLAN_CONN_DIAG_CUSTOM_EVENT: Additional WLAN logs
+ * @WLAN_CONN_DIAG_EAP_START_EVENT: EAPOL start frame
  * @WLAN_CONN_DIAG_MAX: MAX tag
  */
 enum qca_conn_diag_log_event_type {
@@ -203,6 +204,7 @@ enum qca_conn_diag_log_event_type {
 	WLAN_CONN_DIAG_EAP_SUCC_EVENT,
 	WLAN_CONN_DIAG_EAP_FAIL_EVENT,
 	WLAN_CONN_DIAG_CUSTOM_EVENT,
+	WLAN_CONN_DIAG_EAP_START_EVENT,
 	WLAN_CONN_DIAG_MAX
 };
 
