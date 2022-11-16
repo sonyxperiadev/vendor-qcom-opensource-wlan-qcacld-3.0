58343df0e4f9a210bace63c75ca0782ebb001114
Chaoli Zhou <quic_zchaoli@quicinc.com>
2022-11-16 16:46:15 +0800

    qcacld-3.0: Fix compile error for CFR
    Fix two compile issues:
    1. wlan_hdd_transport_mode_cfg defined not used if
    enhance CFR not enabled.
    2. incompatible types for hdd_objmgr_get_vdev_by_user
    function.
    
    Change-Id: If52fe33f9181f6e66d0c1af3c02a71a9df175281
    CRs-Fixed: 3338995

--- core/hdd/src/wlan_hdd_cfr.c
+++ core/hdd/src/wlan_hdd_cfr.c
@@ -78,6 +78,7 @@ const struct nla_policy cfr_config_policy[
 						.type = NLA_U32},
 };
 
+#ifdef WLAN_ENH_CFR_ENABLE
 static void
 wlan_hdd_transport_mode_cfg(struct wlan_objmgr_pdev *pdev,
 			    uint8_t vdev_id, uint32_t pid,
@@ -103,8 +104,6 @@ wlan_hdd_transport_mode_cfg(struct wlan_objmgr_pdev *pdev,
 	else
 		pa->nl_cb.cfr_nl_cb = NULL;
 }
-
-#ifdef WLAN_ENH_CFR_ENABLE
 
 #define DEFAULT_CFR_NSS 0xff
 #define DEFAULT_CFR_BW  0xf
@@ -457,11 +456,10 @@ wlan_cfg80211_peer_cfr_capture_cfg_adrastea(struct hdd_adapter *adapter,
 			QCA_WLAN_VENDOR_ATTR_PEER_CFR_ENABLE]);
 	}
 
-	vdev = adapter->vdev;
-	status = hdd_objmgr_get_vdev_by_user(vdev, WLAN_CFR_ID);
-	if (QDF_IS_STATUS_ERROR(status)) {
-		hdd_err("failed to get vdev");
-		return status;
+	vdev = hdd_objmgr_get_vdev_by_user(adapter, WLAN_CFR_ID);
+	if (!vdev) {
+		hdd_err("can't get vdev");
+		return -EINVAL;
 	}
 
 	pdev = wlan_vdev_get_pdev(vdev);
@@ -587,11 +585,10 @@ wlan_cfg80211_peer_cfr_capture_cfg_adrastea(struct hdd_adapter *adapter,
 	if (tb[id])
 		is_start_capture = nla_get_flag(tb[id]);
 
-	vdev = adapter->vdev;
-	status = hdd_objmgr_get_vdev_by_user(vdev, WLAN_CFR_ID);
-	if (QDF_IS_STATUS_ERROR(status)) {
-		hdd_err("failed to get vdev");
-		return status;
+	vdev = hdd_objmgr_get_vdev_by_user(adapter, WLAN_CFR_ID);
+	if (!vdev) {
+		hdd_err("can't get vdev");
+		return -EINVAL;
 	}
 
 	pdev = wlan_vdev_get_pdev(vdev);
