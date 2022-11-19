c648b4bd1da946f3609e3ef49eebb5805bc89f1d
Madan Koyyalamudi <quic_mkoyyala@quicinc.com>
2022-11-19 05:47:45 -0800

    Release 5.2.1.45Y
    Release 5.2.1.45Y
    
    Change-Id: I3e39432d7403ae5eda07b173a40dbaabfa03abee
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "X"
+#define QWLAN_VERSION_EXTRA            "Y"
 #define QWLAN_VERSION_BUILD            45
 
-#define QWLAN_VERSIONSTR               "5.2.1.45X"
+#define QWLAN_VERSIONSTR               "5.2.1.45Y"
 
 #endif /* QWLAN_VERSION_H */
