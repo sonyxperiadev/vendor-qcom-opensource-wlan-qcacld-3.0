d10945a7b3b55a4c26f94e73b0afa4d9c912c45f
Asutosh Mohapatra <quic_asutmoha@quicinc.com>
2022-12-01 19:40:48 +0530

    qcacld-3.0: Handle timer initialization gracefullly
    Currently hdd_medium_assess_init() and hdd_medium_assess_deinit()
    are not paired with event the events eSAP_START_BSS_EVENT and
    eSAP_STOP_BSS_EVENT respectively. Hence multiple call traces
    can be seen for eSAP_STOP_BSS_EVENT if eSAP_START_BSS_EVENT is
    not invoked from __wlan_hdd_cfg80211_start_ap or SSR sequence.
    
    To address this issue invoke hdd_medium_assess_init()
    for eSAP_START_BSS_EVENT only from hdd_hostapd_sap_event_cb
    if timer is not initialized already.
    
    Change-Id: I361e371c8b59a226fe6cae97c6f66bec46eeeb0f
    CRs-Fixed: 3349485

--- core/hdd/src/wlan_hdd_hostapd.c
+++ core/hdd/src/wlan_hdd_hostapd.c
@@ -2048,6 +2048,9 @@ QDF_STATUS hdd_hostapd_sap_event_cb(struct sap_event *sap_event,
 			      ap_ctx->operating_chan_freq,
 			      sap_config->ch_params.ch_width);
 
+		if (hdd_medium_access_state() == true)
+			hdd_medium_assess_init();
+
 		sap_config->ch_params = ap_ctx->sap_context->ch_params;
 		sap_config->sec_ch_freq = ap_ctx->sap_context->sec_ch_freq;
 
@@ -7896,7 +7899,6 @@ static int __wlan_hdd_cfg80211_start_ap(struct wiphy *wiphy,
 		}
 	}
 
-	hdd_medium_assess_init();
 	goto success;
 
 err_start_bss:
