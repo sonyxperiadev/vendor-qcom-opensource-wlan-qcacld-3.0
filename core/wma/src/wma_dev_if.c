f28de248667d94420be63179e54bdd5c5c54c790
Chaoli Zhou <quic_zchaoli@quicinc.com>
2022-12-02 18:33:25 +0800

    qcacld-3.0: Change kmsg log to debug log
    Change error level log to info level since it
    is not a real error report.
    
    Change-Id: I2698216b67d986fd8e5465d47ca5700e3cc671d3
    CRs-Fixed: 3351802

--- core/wma/src/wma_dev_if.c
+++ core/wma/src/wma_dev_if.c
@@ -415,7 +415,7 @@ QDF_STATUS wma_vdev_detach_callback(struct vdev_delete_response *rsp)
 
 	iface = &wma->interfaces[rsp->vdev_id];
 
-	wma_err("vdev del response received for VDEV_%d", rsp->vdev_id);
+	wma_info("vdev del response received for VDEV_%d", rsp->vdev_id);
 	iface->del_staself_req = NULL;
 
 	if (iface->roam_scan_stats_req) {
