c1a3976958b0c3a5acdb4bb184a2d91a425d01b9
Rahul Choudhary <quic_rahulc@quicinc.com>
2023-06-02 01:05:43 -0700

    Release 5.2.1.58X
    Release 5.2.1.58X
    
    Change-Id: I8d454014108d6c4ed036934d476f8b87e8de5d24
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "W"
+#define QWLAN_VERSION_EXTRA            "X"
 #define QWLAN_VERSION_BUILD            58
 
-#define QWLAN_VERSIONSTR               "5.2.1.58W"
+#define QWLAN_VERSIONSTR               "5.2.1.58X"
 
 #endif /* QWLAN_VERSION_H */
