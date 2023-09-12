e1b0a8b19272661bc84937fa2c4f609d6c470052
Srinivas Dasari <quic_dasaris@quicinc.com>
2023-09-12 23:38:47 +0530

    qcacld-3.0: Reset the chan_switch_in_progress in failure cases
    When host driver receives CSA event from firmware, lim posts
    VDEV_RESTERT request with the CSA info. But the channel
    switch operation may fail in further APIs due to any
    reason(e.g. channel not valid for switch). Failure status is not
    forwarded till lim in such cases as vdev SM always returns
    true(success) when it handles the message.
    So, the flag doesn't get reset in such cases.
    Reset the flag in lim handler of VDEV_RESTART request when it
    fails to send VDEV_RESTART command to firmware.
    
    Change-Id: Iceeabd9cab46107ce7e2d14af121b0155c27673c
    CRs-Fixed: 3602751

--- core/mac/src/pe/lim/lim_utils.h
+++ core/mac/src/pe/lim/lim_utils.h
@@ -533,11 +533,11 @@ void lim_process_channel_switch(struct mac_context *mac, uint8_t vdev_id);
  *
  * This function changes the current operating channel frequency.
  *
- * return NONE
+ * return qdf_status
  */
-void lim_switch_primary_channel(struct mac_context *mac,
-				uint32_t new_channel_freq,
-				struct pe_session *pe_session);
+QDF_STATUS lim_switch_primary_channel(struct mac_context *mac,
+				      uint32_t new_channel_freq,
+				      struct pe_session *pe_session);
 
 /**
  * lim_switch_primary_secondary_channel() - switch primary and secondary
@@ -551,10 +551,10 @@ void lim_switch_primary_channel(struct mac_context *mac,
  *  then we must set this new channel in session context and
  *  assign notify LIM of such change.
  *
- * @return NONE
+ * @return qdf_status
  */
-void lim_switch_primary_secondary_channel(struct mac_context *mac,
-					  struct pe_session *pe_session);
+QDF_STATUS lim_switch_primary_secondary_channel(struct mac_context *mac,
+						struct pe_session *pe_session);
 
 void lim_update_sta_run_time_ht_capability(struct mac_context *mac,
 		tDot11fIEHTCaps *pHTCaps);
